f4a8485ea778e55c33441f0ed9b360ec3aa01e4a
unknown <gabrieldossantosleite2004@gmail.com>
2023-08-22 22:08:10 -0300

    primeiro commit

---
+++ classes/Usuario.php
@@ -0,0 +1,66 @@
+<?php
+
+include('conexao/conexao.php');
+
+$db = new Conexao();
+
+class Usuario{
+    private $conn;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function cadastrar($nome, $email, $senha, $confSenha)
+    {
+        if($senha == $confSenha){
+
+            $emailExistente = $this->verificacaoEmailExistente($email);
+            if($emailExistente){
+                print "<script>alert('Email já cadastrado')</script>";
+                return false;
+            }
+
+        $senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)";
+        
+        $stmt = $this->conn->prepare($sql);
+        $stmt -> bindValue(1, $nome);
+        $stmt -> bindValue(2, $email);
+        $stmt -> bindValue(3, $senhaCriptografada);
+        $result = $stmt -> execute();
+
+        return $result;
+
+        }else{
+            return false;
+        }
+    }
+
+    private function verificacaoEmailExistente($email){
+        $sql = "SELECT COUNT(*) from usuario WHERE email = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(1,$email);
+        $stmt->execute();
+
+        return $stmt->fetchColumn() > 0;
+    } 
+
+    public function logar($nome, $senha){
+        $sql = "SELECT * FROM usuario WHERE nome = :nome";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':nome', $nome);
+        $stmt->execute();
+
+    if($stmt->rowCount() == 1){
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(password_verify($senha,$usuario['senha'])){
+            return true;
+        }
+    }
+    }
+}
+
+
+?>
